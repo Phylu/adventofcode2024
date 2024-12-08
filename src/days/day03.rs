726eed0dc97ceb3ad7b17fe7b49e3584f8a6165c
Janosch Maier <janosch.maier@ottonova.de>
2024-12-08 20:58:12 +0100

    feat: add day 3 challenge 2

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,5 +1,4 @@
-use std::result;
-
+use itertools::join;
 use regex::Regex;
 
 const MULTIPLY: &str = r"mul\((?P<x>\d+),(?P<y>\d+)\)";
@@ -23,9 +22,13 @@ fn task1(content: &String) -> String {
 }
 
 fn task2(content: &String) -> String {
-    let input = prepare_input(content);
+    let input = prepare_input_2(content);
 
     let mut result = 0;
+
+    for (x, y) in input {
+        result += x * y;
+    }
 
     return result.to_string();
 }
@@ -49,6 +52,44 @@ fn prepare_input(content: &String) -> Vec<(i32, i32)> {
     result
 }
 
+fn prepare_input_2(content: &String) -> Vec<(i32, i32)> {
+
+    let mut result  = Vec::new();
+    let re = Regex::new(MULTIPLY).unwrap();
+    let mut input = join(content.lines(), "");
+
+        let mut collection = true;
+
+        while input != "" {
+            let delimiter;
+            if collection {
+                delimiter = "don't()";
+            } else {
+                delimiter = "do()";
+            }
+            let (first, second) = input.split_once(delimiter).unwrap_or((&input, ""));
+
+            // println!("{} / {}", first, second);
+
+            if collection {
+                let captures = re.captures_iter(first);
+
+                for capture in captures {
+                    let x: i32 = capture.name("x").unwrap().as_str().parse().unwrap();
+                    let y: i32 = capture.name("y").unwrap().as_str().parse().unwrap();
+                    result.push((x, y));
+                }
+            }
+
+            input = second.to_owned();
+            collection = !collection;
+
+            // println!("collection: {}", collection);
+        }
+
+    result
+}
+
 
 #[test]
 fn test_task1() {
@@ -59,6 +100,6 @@ fn test_task1() {
 
 #[test]
 fn test_task2() {
-    let content = std::fs::read_to_string("input_test/3.txt").unwrap(); 
-    assert_eq!(task2(&content), "31");
+    let content = std::fs::read_to_string("input_test/3_1.txt").unwrap(); 
+    assert_eq!(task2(&content), "48");
 }
